76332d656615941cfb5a83e520d67a2ba262b392
Francois Dagostini <francois.dagostini@gmail.com>
2025-06-17 09:40:37 -0400

    update

--- src/app/[...slug]/page.js
+++ src/app/[...slug]/page.js
@@ -1,5 +1,6 @@
 import { StoryblokStory } from '@storyblok/react/rsc';
 import { getStoryblokApi } from '@/lib/storyblok';
+import getSbVersion from '../../utils/getSbVersion';
 
 export default async function Page({ params }) {
 const { slug } = await params;
@@ -8,7 +9,7 @@ const fullSlug = slug ? slug.join('/') : 'home';
 
 const storyblokApi = getStoryblokApi();
 let { data } = await storyblokApi.get(`cdn/stories/${fullSlug}`, {
-	version: 'draft',
+	version: getSbVersion,
 });
 
 return <StoryblokStory story={data.story} />;
